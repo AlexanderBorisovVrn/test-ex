9b118eb7f60e188023316a904533b1dd9873821c
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2022-07-27 21:19:30 +0300

    firs iteration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{docs}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+      return {
+        data:null
+      }
+  },
+  computed:{
+    docs(){
+      return this.data?.docs.map(data=>data)
+    }
+  },
+  mounted(){
+    axios.get('https://openlibrary.org/search.json?q=mr+fox').then(
+      res=>this.data=res.data
+    )
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
